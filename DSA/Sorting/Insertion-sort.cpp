73072d65e94135cd2b7523f52c8634cc3ccf78bc
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-02 10:37:45 +0530

    Add files via upload

---
+++ DSA/Sorting/Insertion-sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int a[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int curr=a[i];
+        int j=i-1;
+        while(j>=0 && a[j]>curr )
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=curr;
+    }
+    for(int i=0;i<n;i++)
+        cout<<a[i];
+}
+
+
+
+
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    insertionSort(a,n);
+    return 0;    
+}
