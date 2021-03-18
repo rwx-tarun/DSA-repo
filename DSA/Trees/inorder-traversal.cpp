e5f8924e2f87a043eae1b986bbd3d354a932b354
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-18 11:17:45 +0530

    Add files via upload

---
+++ DSA/Trees/inorder-traversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    Node *root = new Node(4);
+    root->left = new Node(2);
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    root->right = new Node(6);
+    root->right->left = new Node(5);
+    root->right->right = new Node(7);
+    inorder(root);
+    return 0;
+}
