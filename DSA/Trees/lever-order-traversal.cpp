bec763d7843eeb15ba923de74da8cfbf43d91e19
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-24 14:16:13 +0530

    Add files via upload

---
+++ DSA/Trees/lever-order-traversal.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void levelOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->key << " ";
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
+}
+
+int main()
+{
+    Node *root = new Node(2);
+    root->left = new Node(3);
+    root->right = new Node(4);
+    root->left->left = new Node(5);
+    root->left->right = new Node(6);
+    root->right->left = new Node(7);
+    root->right->right = new Node(8);
+    levelOrder(root);
+    return 0;
+}
