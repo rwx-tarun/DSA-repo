9787ee68a3722adddd80dc4dcf0269dfb4e1ffe3
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-30 17:43:35 +0530

    Add files via upload

---
+++ DSA/Trees/Max-Width-Binary_tree.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int maxWidth(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    queue<Node *> q;
+    int res = 0;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        int cnt = q.size();
+        res = max(res, cnt);
+        for (int i = 0; i < cnt; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    Node *root = new Node(2);
+    root->left = new Node(3);
+    root->right = new Node(4);
+    root->left->left = new Node(5);
+    root->left->right = new Node(6);
+    root->right->left = new Node(7);
+    cout << maxWidth(root);
+    return 0;
+}
