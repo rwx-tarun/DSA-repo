0d5fe46ab32dca2066066254450e7de465c41cd7
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-03-31 11:00:54 +0530

    Add files via upload

---
+++ DSA/Trees/right-view.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void leftView(Node *root)
+{
+    if (root == NULL)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        int cnt = q.size();
+        for (int i = 0; i < cnt; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+            if (i == cnt - 1)
+                cout << curr->key << " ";
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(2);
+    root->left = new Node(3);
+    root->right = new Node(4);
+    root->left->left = new Node(5);
+    root->left->right = new Node(6);
+    root->right->left = new Node(7);
+    root->right->right = new Node(8);
+    leftView(root);
+    return 0;
+}
