611903db3a01d882687305fa6961541f18e82720
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-04-26 11:58:04 +0530

    Create Linked-list-implementation.cpp

---
+++ DSA/Linked-List/Linked-list-implementation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void display(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(5);
+    head->next = new Node(4);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(2);
+    head->next->next->next->next = new Node(1);
+    display(head);
+    return 0;
+}
