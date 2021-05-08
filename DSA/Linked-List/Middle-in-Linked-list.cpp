c98ac9a7d9674c47c98f6706e85f9116e8bfbfdb
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-05-08 17:35:41 +0530

    Add files via upload

---
+++ DSA/Linked-List/Middle-in-Linked-list.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void display(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+}
+
+int middle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow->data;
+}
+
+int main()
+{
+    Node *head = new Node(5);
+    head->next = new Node(4);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(2);
+    head->next->next->next->next = new Node(1);
+    int mid = middle(head);
+    display(head);
+
+    cout << endl
+         << mid << endl;
+    ;
+    return 0;
+}
