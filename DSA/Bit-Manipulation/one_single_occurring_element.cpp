e87d45919aa51267224d770a76a11cb94e686d58
rwx-tarun <78414267+rwx-tarun@users.noreply.github.com>
2021-11-19 23:36:45 +0530

    Update one_single_occurring_element.cpp

--- DSA/Bit-Manipulation/one_single_occurring_element.cpp
+++ DSA/Bit-Manipulation/one_single_occurring_element.cpp
@@ -4,3 +4,16 @@ int Solution::singleNumber(const vector<int> &A) {
         res= res^x;
     return res;
 }
+
+
+// int solve(vector<int>A)
+// {
+//     sort(A.begin(),A.end());
+//     int n = A.size();
+//     for(int i=1;i<n;i=i+2)
+//     {
+//         if(A[i-1]!=A[i])
+//             return A[i];
+//     }
+//     return A[n-1];
+// }
